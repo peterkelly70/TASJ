7bad9b4d32532a8e4f5a0eadcb0fc9735266d7c8
Peter Kelly <peter@computer-wizard.com.au>
2025-03-31 18:37:59 +0800

    13bbbbd - Snapshot from iterations

--- controller/data_download_controller.py
+++ controller/data_download_controller.py
@@ -29,8 +29,9 @@ class DataDownloadController:
         if self.console_view:
             self.console_view.clear_text()
             self.console_view.append_text("Starting download...\n")
-            self.console_view.enable_cancel(True)
-            self.console_view.update_progress(0)
+            self.console_view.show_progress_bar(True)
+            self.console_view.update_progress_bar(0)
+            self.console_view.enable_cancel_button(True)
         
         # Clear the cancel event
         self.cancel_event.clear()
@@ -48,13 +49,13 @@ class DataDownloadController:
     def cancel_download(self):
         """Cancel the ongoing download process."""
         if self.console_view:
-            self.console_view.enable_cancel(False)
+            self.console_view.enable_cancel_button(False)
         self.cancel_event.set()
 
     def update_progress(self, progress: int, message: str):
         """Update the progress bar and status message."""
         if self.console_view:
-            self.console_view.update_progress(progress)
+            self.console_view.update_progress_bar(progress)
             self.console_view.append_text(f"Progress: {progress}% - {message}\n")
 
     def _monitor_queue(self):
@@ -72,61 +73,5 @@ class DataDownloadController:
             self.update_progress(progress, message)
 
         if self.console_view:
-            self.console_view.enable_cancel(False)
-
-    def set_console_view(self, console_view):
-        """Set the console view for progress and cancel functionality."""
-        self.console_view = console_view
-        self.cancel_button = console_view.cancel_button
-        self.cancel_button.clicked.connect(self.cancel_event.set)
-        self.progress_bar = console_view.progress_bar
-
-    def start_download(self):
-        """Start the data download process."""
-        if self.console_view:
-            self.console_view.clear_text()
-            self.console_view.append_text("Starting download...\n")
-            self.console_view.enable_cancel(True)
-            self.console_view.update_progress(0)
-        
-        # Clear the cancel event
-        self.cancel_event.clear()
-
-        # Start the download process in a separate process
-        process = multiprocessing.Process(
-            target=download_data_task,
-            args=(self.db_instance.db_type, self.queue, self.cancel_event)
-        )
-        process.start()
-
-        # Start monitoring the queue
-        self._monitor_queue()
-
-    def cancel_download(self):
-        """Cancel the ongoing download process."""
-        if self.console_view:
-            self.console_view.enable_cancel(False)
-        self.cancel_event.set()
-
-    def update_progress(self, progress: int, message: str):
-        """Update the progress bar and status message."""
-        if self.console_view:
-            self.console_view.update_progress(progress)
-            self.console_view.append_text(f"Progress: {progress}% - {message}\n")
-
-    def _monitor_queue(self):
-        """Monitor the queue for progress updates."""
-        while not self.cancel_event.is_set():
-            try:
-                progress, message = self.queue.get_nowait()
-                self.update_progress(progress, message)
-            except Empty:
-                time.sleep(0.1)
-
-        # Final update
-        if not self.queue.empty():
-            progress, message = self.queue.get()
-            self.update_progress(progress, message)
-
-        if self.console_view:
-            self.console_view.enable_cancel(False)
+            self.console_view.enable_cancel_button(False)
+            self.console_view.show_progress_bar(False)
